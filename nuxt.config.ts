0a7151a86adad7d162fa98f5a39029d8d40e23f5
Daniel Muszkiet <41920001+danielmuszkiet@users.noreply.github.com>
2025-12-27 15:51:49 +0100

    Tl 14 (#53)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 
-import "./lib/env";
+import "./app/lib/env";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
