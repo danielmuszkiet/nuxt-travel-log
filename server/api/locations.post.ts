dd8ffc5f33a9bea7746f38c3f9e64b81908d5ff6
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2026-01-02 14:52:39 +0100

    add location to db

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,23 @@
+import type { LibsqlError } from "@libsql/client";
+
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "../lib/db";
 import { InsertLocationSchema, location } from "../lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+async function generateUniqueSlug(name: string) {
+  const slug = slugify(name);
+  let suffix = "";
+
+  while (await db.query.location.findFirst({ where: eq(location.slug, slug + suffix) })) {
+    suffix = `-${nanoid()}`;
+  }
+
+  return slug + suffix;
+}
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -26,18 +44,49 @@ export default defineEventHandler(async (event) => {
         return errors;
       }, {} as Record<string, string>);
 
-    throw sendError(event, createError({
+    throw createError({
       statusCode: 422,
       statusMessage,
       data,
-    }));
+    });
   }
 
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where: and(
+      eq(location.userId, event.context.user.id),
+      eq(location.name, result.data.name),
+    ),
+  });
 
-  return created;
+  if (existingLocation) {
+    throw createError({
+      statusCode: 409,
+      statusMessage: "Location with this name already exists for you!",
+    });
+  }
+
+  const slug = await generateUniqueSlug(result.data.name);
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+
+    return created;
+  }
+  catch (e) {
+    const error = e as LibsqlError;
+    if (error.cause instanceof Error) {
+      if (error.cause.message === "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.slug") {
+        throw sendError(event, createError({
+          statusCode: 409,
+          statusMessage: "Slug must be unique (the location name is used to generate the slug).",
+        }));
+      }
+    }
+
+    throw e; // optional
+  }
 });
